19768a6f5d7b7d713d41e2f049423d7ab4982cca
baviddak <dbak.db@gmail.com>
2022-06-06 15:37:21 -0400

    update

--- 06_processes/ex03/build_my_unsetenv.sh
+++ 06_processes/ex03/build_my_unsetenv.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-
-
-gcc -Wall -o my_unsetenv my_unsetenv.c ../../../tlpi-dist/libtlpi.a -I../../../tlpi-dist/lib
+gcc -Wall -o my_unsetenv.exe my_unsetenv.c ../../../tlpi-dist/libtlpi.a -I../../../tlpi-dist/lib
