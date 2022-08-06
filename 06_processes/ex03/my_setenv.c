a102024869a40b095cbd9dc81f5f613338ca8ab8
baviddak <dbak.db@gmail.com>
2022-08-06 00:01:34 -0400

    formatting

--- 06_processes/ex03/my_setenv.c
+++ 06_processes/ex03/my_setenv.c
@@ -14,58 +14,59 @@
 extern char **environ;
 
 int my_setenv(const char *name, const char *value, int overwrite){
+	
+	/* Since local_name and local_value are allocated on the heap, we don't have to
+	* worry about the environment variables disappearing once the functions return
+	*/
 
-  /* Since local_name and local_value are allocated on the heap, we don't have to 
-   * worry about the environment variables disappearing once the functions return 
-   */
-  
-  char * nv_pair = (char *)malloc(sizeof(name) + sizeof(value) + sizeof(char));
-  char * equal = "=";
-  strcpy(nv_pair, name);
-  strcat(nv_pair, equal);
-  strcat(nv_pair, value);
+	char * nv_pair = (char *)malloc(sizeof(name) + sizeof(value) + sizeof(char));
+	char * equal = "=";
+	strcpy(nv_pair, name);
+	strcat(nv_pair, equal);
+	strcat(nv_pair, value);
+	
+	/* Do not change the environment variable if it already exists and the override is 0 */
+	
+	if(overwrite != 0){
+		if(putenv(nv_pair) != 0){
+			return(-1);
+		}
+	} else {
+		if(getenv(name) == NULL){
+			if(putenv(nv_pair) != 0){
+				return(-1);
+			}
+		}
+	}
 
-  /* Do not change the environment variable if it already exists and the override is 0 */
-
-  if(overwrite != 0){
-    if(putenv(nv_pair) != 0){
-      return(-1);
-    }
-  }else{
-    if(getenv(name) == NULL){
-      if(putenv(nv_pair) != 0){
-	return(-1);
-      }  
-    }
-  }
-  return(0);
+	return(0);
 }
 
 int main(int argc, char * argv[]){
-
-  char **p;
-  for(p = environ; *p != NULL; p++){
-    puts(*p);
-  }
-
-  my_setenv("ENV_VAR_1", "bitch", 0); /* Will be added, then over written by last call */
-  my_setenv("ENV_VAR_1", "okay", 0); /* Will not over write the first call */
-
-  printf("--------------------------------\n\n");
-  printf("--------------------------------\n\n");
-  for(p = environ; *p != NULL; p++){
-    puts(*p);
-  }
-  
-  my_setenv("ENV_VAR_2", "dun matter", 1); /* Will simply be added */
-  my_setenv("ENV_VAR_1", "this", 1); /* Will overwrite the first call */
-
-  printf("--------------------------------\n\n");
-  printf("--------------------------------\n\n");
-  for(p = environ; *p != NULL; p++){
-    puts(*p);
-  }
-  
-  return(EXIT_SUCCESS);
+	
+	char **p;
+	for(p = environ; *p != NULL; p++){
+		puts(*p);
+	}
+	
+	my_setenv("ENV_VAR_1", "brap", 0); /* Will be added, then over written by last call */
+	my_setenv("ENV_VAR_1", "okay", 0); /* Will not over write the first call */
+	
+	printf("--------------------------------\n\n");
+	printf("--------------------------------\n\n");
+	for(p = environ; *p != NULL; p++){
+		puts(*p);
+	}
+	
+	my_setenv("ENV_VAR_2", "dun matter", 1); /* Will simply be added */
+	my_setenv("ENV_VAR_1", "this", 1); /* Will overwrite the first call */
+	
+	printf("--------------------------------\n\n");
+	printf("--------------------------------\n\n");
+	for(p = environ; *p != NULL; p++){
+		puts(*p);
+	}
+	
+	return(EXIT_SUCCESS);
 }
 
