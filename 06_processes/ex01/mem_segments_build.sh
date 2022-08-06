2276b173cced2b6ca45cd63786b991ab7b0da145
baviddak <dbak.db@gmail.com>
2022-08-05 23:09:25 -0400

    add answer

--- 06_processes/ex01/mem_segments_build.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-gcc -Wall -o mem_segments.exe mem_segments.c ../../tlpi-dist/libtlpi.a -I../../tlpi-dist/lib 
--- 06_processes/ex03/build_my_unsetenv.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-gcc -Wall -o my_unsetenv.exe my_unsetenv.c ../../../tlpi-dist/libtlpi.a -I../../../tlpi-dist/lib
