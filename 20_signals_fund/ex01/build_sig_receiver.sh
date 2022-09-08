c4bffc80053d4958c3b9dcc5d39e637ca0657b28
baviddak <dbak.db@gmail.com>
2022-09-07 23:09:38 -0400

    remove unfinished stuff

--- 20_signals_fund/ex01/build_sig_receiver.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-gcc -Wall -o sig_receiver_sigaction.exe sig_receiver_sigaction.c ../../../tlpi-dist/libtlpi.a -I../../../tlpi-dist/lib
-gcc -Wall -o sig_sender.exe sig_sender.c ../../../tlpi-dist/libtlpi.a -I../../../tlpi-dist/lib
