4cf2065b7f68ec5b548be389f068c088ca43e63e
baviddak <dbak.db@gmail.com>
2022-10-16 20:25:21 -0400

    implement realpath

--- 18_dirs_links/ex03/my_realpath.c
+++ 18_dirs_links/ex03/my_realpath.c
@@ -2,6 +2,8 @@
 
 /* Usage: ./my_realpath.exe <symlinkpath> */
 
+#include <sys/stat.h>
+#include <limits.h>
 
 #include "tlpi_hdr.h"
 
@@ -9,10 +11,30 @@
 #define PATH_MAX 100
 #endif // PATH_MAX
 
-// char * my_realpath (const char *pathname, char *resolved_path) {
-// 	// will have to check the i-node table
+char * my_realpath (const char *pathname, char *resolved_path) {
+	int num_bytes;
 
-// }
+	// will have to check the i-node table
+	struct stat *stat_buf = (struct stat *)malloc(sizeof(struct stat));
+
+	// get the i-node
+	if (lstat(pathname, stat_buf) == -1) {
+		err_exit("lstat");
+	}
+
+	// check if it is a symlink
+	if (!S_ISLNK(stat_buf->st_mode)) {
+		fatal("Not a symbolic link");
+	}
+
+	// copy symlink string to by resolved_path
+	num_bytes = readlink(pathname, resolved_path, PATH_MAX);
+
+	// null-terminate the string
+	resolved_path[num_bytes] = '\0';
+
+	return resolved_path;
+}
 
 int main (int argc, char *argv[]) {
 
@@ -22,11 +44,11 @@ int main (int argc, char *argv[]) {
 	}
 
 	char *symlink_path = argv[1];
-	char my_realpath[PATH_MAX];
+	char realpath[PATH_MAX];
 
-	realpath(symlink_path, my_realpath);
-
-	printf("The real path is: %s\n", my_realpath);
+	// test our function
+	my_realpath(symlink_path, realpath);
+	printf("The real path is: %s\n", realpath);
 
 	return (EXIT_SUCCESS);
 }
