8a3e2bb149519ff822f911874c49aeedd304b7aa
David Bak <dbak.db@gmail.com>
2023-07-28 22:54:52 -0400

    convert spaces to tabs

--- 23_timers_sleep/ex01/my_alarm.c
+++ 23_timers_sleep/ex01/my_alarm.c
@@ -13,40 +13,40 @@
 #include "tlpi_hdr.h"
 
 unsigned int my_alarm(unsigned int seconds){
-    int which = ITIMER_REAL;
-    struct itimerval new_val;
-    struct itimerval old_val;
+	int which = ITIMER_REAL;
+	struct itimerval new_val;
+	struct itimerval old_val;
 
-    new_val.it_value.tv_sec = (time_t)seconds;
-    new_val.it_value.tv_usec = 0;
-    
-    if (setitimer(which, &new_val, &old_val) == -1) {
-        return 0;
-    } else {
-        return (unsigned int)old_val.it_value.tv_sec;
-    }
+	new_val.it_value.tv_sec = (time_t)seconds;
+	new_val.it_value.tv_usec = 0;
+	
+	if (setitimer(which, &new_val, &old_val) == -1) {
+		return 0;
+	} else {
+		return (unsigned int)old_val.it_value.tv_sec;
+	}
 }
 
 int main(int argc, char * argv[]) {
 
-    // Validate the command line args
-    if(argc != 2 || strcmp(argv[1], "--help") == 0){
+	// Validate the command line args
+	if(argc != 2 || strcmp(argv[1], "--help") == 0){
 		usageErr("%s \narg count is %d\n", argv[0], argc);
 	}
 
-    int seconds = atoi(argv[1]);
-    if ((seconds <= 0) || seconds > 20) {
-        errExit("atoi");
-    }
+	int seconds = atoi(argv[1]);
+	if ((seconds <= 0) || seconds > 20) {
+		errExit("atoi");
+	}
 
-    my_alarm(seconds);
+	my_alarm(seconds);
 
-    int prev_clock = clock();
+	int prev_clock = clock();
 
-    while(1){
-        if ((clock() - prev_clock) / CLOCKS_PER_SEC > 1){
-            printf("\nStill running...");
-            prev_clock = clock();
-        }
-    }
+	while(1){
+		if ((clock() - prev_clock) / CLOCKS_PER_SEC > 1){
+			printf("\nStill running...");
+			prev_clock = clock();
+		}
+	}
 }
