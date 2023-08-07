9c9c2cad84e7c04b2ad8fb74910133a5a5a16b6e
David Bak <dbak.db@gmail.com>
2023-08-07 17:54:57 -0400

    additions

---
+++ 26_child_processes/ex01/child_init.c
@@ -0,0 +1,24 @@
+/* Write a program to verify that when a child's parent terminates, a call to 
+ * getppid() returns 1 (the process ID of init - systemd in newer systems).
+ */
+
+#include "tlpi_hdr.h"
+
+int main() {
+
+	printf("The parent process ID is: %d\n", getpid());
+
+	switch(fork()){
+		case -1:
+			// bad
+		case 0:
+			// The child process
+			sleep(10);
+			printf("The parent process ID after terminating is: %d\n", getppid());
+			_exit(0);
+		default:
+			// Parent process continues
+			_exit(0);
+	}
+	return(EXIT_SUCCESS);
+}
