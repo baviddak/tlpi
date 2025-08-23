b278dfa023d45205b0c0700fef0fe02a922b4185
David Bak <dbak.db@gmail.com>
2025-08-22 22:34:05 -0400

    add partial solution

--- 44_pipes_fifos/ex01/pipes.c
+++ 44_pipes_fifos/ex01/pipes.c
@@ -7,4 +7,99 @@
  * echoes it on standard output before continuing around the loop once more.
  */
 
-int main () {}
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tlpi_hdr.h"
+
+#define BUFFER_MAX 120
+
+
+void lower_to_upper(char *lower, char *upper, int size) {
+
+    // int i;
+    for (int i = 0; i < size; i++ ) {
+        switch(lower[i]) {
+            case
+        }        
+
+
+    }
+
+}
+
+int main () {
+
+    int num_read;
+
+    /* 0 is the read end; 1 is the write end */
+
+    /* Create our pipe file descriptors */
+    int parent_to_child_fd[2];
+    int child_to_parent_fd[2];
+
+    /* Create the pipes */
+
+    if (pipe(parent_to_child_fd) == -1) {
+        errExit("pipe");
+    }
+
+    if(pipe(child_to_parent_fd) == -1) {
+        errExit("pipe");
+    }
+
+    /* Buffer for the text read in */
+    char BUFFER[BUFFER_MAX];
+    char BUFFER_UPPER_CASE[BUFFER_MAX];
+
+    switch(fork()) {
+        case -1:
+            errExit("fork");
+        case 0:
+
+            /* Child process */
+            if (close(parent_to_child_fd[1]) == -1) {
+                // Close the write end of the parent to child pipe
+                errExit("close");
+            }
+            if (close(child_to_parent_fd[0] == -1)) {
+                // Close the read end of the child to parent pipe
+                errExit("close");
+            }
+
+            /* Close the*/
+
+        case 1:
+
+            /* Parent process */
+            if (close(parent_to_child_fd[0]) == -1) {
+                // Close the read end of the parent to child pipe
+                errExit("close");
+            }
+            if (close(child_to_parent_fd[1] == -1)) {
+                // Close the write end of the child to parent pipe
+                errExit("close");
+            }
+
+            // Read from stdin - pass to the child process
+            for(;;) {
+                num_read = read(STDIN_FILENO, BUFFER, BUFFER_MAX);
+
+                if (num_read == -1 ) {
+                    errExit("read");
+                }
+                if (num_read == 0) {
+                    break;
+                }
+                if (write(parent_to_child_fd[1]), BUFFER, BUFFER_MAX) == -1) {
+                    errExit("write")
+                }
+
+
+
+            }
+
+    }
+
+}
