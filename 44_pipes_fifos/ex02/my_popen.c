d774afc133f1c6f1eaa6d013f2bf10c82e19c17d
David Bak <dbak.db@gmail.com>
2025-09-06 15:32:52 -0400

    save some progress

--- 44_pipes_fifos/ex02/my_popen.c
+++ 44_pipes_fifos/ex02/my_popen.c
@@ -17,10 +17,35 @@ int main() {
 
 FILE *my_popen(const char *command, const char *mode) {
 
+	/* Before anything, validate the mode*/
+	if (strcmp(mode, "r") != 0 || strcmp(mode, "w") != 0) {
+		return NULL;
+	}
 
+	/* Firstly, create a pipe */
+	int pfd[2];
+
+	if (pipe(pfd) == -1) {
+		errExit("pipe");
+	}
+
+
+
+
+	/* Secondly, fork the process */
+
+
+	/* Thirdly, from the child process, if the mode is r, 
+	 * bind the write end of the pipe to the STDIN of the calling process?
+	 * if the mode is w, bind the read end to .... 
+	 */
+
+	/* Create a new filestream */
 
 }
 
 int my_pclose(FILE *){
+
+	/* Simply close the FD associated with this FILE stream */
 
 }
