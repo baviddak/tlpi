f6ab7a2440e4870a29ea7d488abc4f65915fd813
David Bak <dbak.db@gmail.com>
2025-09-06 15:05:26 -0400

    add solution for popen question

---
+++ 44_pipes_fifos/ex02/my_popen.c
@@ -0,0 +1,26 @@
+/* Implement popen() and pclose(). */
+#include "tlpi_hdr.h"
+
+/* Rename these to my_popen and my_pclose to avoid a double definition error */
+
+FILE *my_popen(const char *, const char *);
+int my_pclose(FILE *);
+
+int main() {
+
+
+	/* Test our functions in main */
+
+
+	return 0;
+}
+
+FILE *my_popen(const char *command, const char *mode) {
+
+
+
+}
+
+int my_pclose(FILE *){
+
+}
