4efbc56fbf3aec2b6dcf40b840b07f1c6f74dd16
David Bak <dbak.db@gmail.com>
2025-10-27 20:26:58 -0400

    fix datagram socket client thing

--- 57_sockets_unix/test/datagram/client.c
+++ 57_sockets_unix/test/datagram/client.c
@@ -11,11 +11,16 @@
 
 int main () {
 
-	int sfd, cl_sfd;
+	int sfd;
 	struct sockaddr_un addr, cl_addr;
 	char buf[BUF_SIZE];
 	int flags = 0;
-	ssize_t bytes_read, bytes_sent;
+	ssize_t bytes_read;
+
+	/* Create a socket file name with the PID */ 
+	char cl_socket_file[BUF_SIZE];
+	memset(cl_socket_file, 0, sizeof(cl_socket_file));
+	snprintf(cl_socket_file, sizeof(cl_socket_file) - 1, "/tmp/socket_cl_file.%ld", (long) getpid());
 
 	/* Create the socket */
 	if ((sfd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1) {
@@ -25,10 +30,9 @@ int main () {
        	}	
 
 	/* Set the contents of the socket address structure. */
-	memset(&addr, 0, sizeof(struct sockaddr_un));
-	addr.sun_family = AF_UNIX;
-	snprintf(addr.sun_path, sizeof(addr.sun_path) - 1, "/tmp/socket_client_file");
-
+	memset(&cl_addr, 0, sizeof(struct sockaddr_un));
+	cl_addr.sun_family = AF_UNIX;
+	snprintf(cl_addr.sun_path, sizeof(cl_addr.sun_path) - 1, cl_socket_file);
 
 	/* Copy the contents of STDIN to the buffer */
 	if ((bytes_read = read(STDIN_FILENO, buf, BUF_SIZE)) == -1) {
@@ -38,24 +42,21 @@ int main () {
 	}
 
 
-	/* Remove the socket file if it exists. */
-	//if (remove(SOCKET_FILE) == -1) {
-	//	if (errno != ENOENT) {
-	//		fprintf(stderr, "Error removing existing socket file\n");
-	//		exit(EXIT_FAILURE);
-	//	}
-	//}
-
-
 	/* Bind the socket to the address. */
-	if (bind(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
+	if (bind(sfd, (struct sockaddr *)&cl_addr, sizeof(struct sockaddr_un)) == -1) {
 		fprintf(stderr, "Error binding the socket to the address\n");
 		fprintf(stderr, "%s\n", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 
+
+	memset(&addr, 0, sizeof(struct sockaddr_un));
+	addr.sun_family = AF_UNIX;
+	snprintf(addr.sun_path, sizeof(addr.sun_path) - 1, SOCKET_FILE);
+
+
 	/* Write the buffer to the socket */
-	if (send(sfd, buf, BUF_SIZE, flags) == -1) {
+	if (sendto(sfd, buf, BUF_SIZE, flags, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
 		fprintf(stderr, "Error in sending data to the socket.\n");
 		fprintf(stderr, "%s\n", strerror(errno));
 		exit(EXIT_FAILURE);
@@ -63,6 +64,10 @@ int main () {
 
 	// fprintf(stdout, "Successfully read %d bytes from the socket. Message %s\n", bytes_read, buf);
 
+	if (remove(cl_socket_file) == -1) {
+		fprintf(stderr, "Error removing the client socket file.\n");
+		exit(EXIT_FAILURE);
+	}
 	exit(EXIT_SUCCESS);
 
 }
