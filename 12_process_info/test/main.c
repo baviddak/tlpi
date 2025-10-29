bbd7d3b201499c82320620cfcb1f613185bdb4fb
David Bak <dbak.db@gmail.com>
2025-10-28 23:20:25 -0400

    add test file for proc folder

---
+++ 12_process_info/test/main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define MAX_PATH 128
+#define MAX_BUFF 2048
+
+#define PATHSTR "/proc/%ld/status"
+
+int main (int argc, char *argv[]) {
+
+	int status_fd;
+	pid_t pid = getpid();
+	char path[MAX_PATH];
+	int flags = O_RDONLY;
+	char buf[MAX_BUFF];
+	memset(buf, 0, sizeof(buf));
+
+	/* fprintf(stdout, "The size of the buffer is %ld\n", sizeof(path)); */
+	memset(path, 0, sizeof(path));
+	snprintf(path, sizeof(path) - 1, PATHSTR, (long) pid);
+
+
+	if ((status_fd = open(path, flags)) < 0) {
+		fprintf(stderr, "Error opening the status file.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (read(status_fd, buf, sizeof(buf) - 1 ) < 0) {
+		fprintf(stderr, "Error in reading from status file.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (write(STDOUT_FILENO, buf, sizeof(buf)) < 0) {
+		fprintf(stderr, "Error printing to standard out.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	exit(EXIT_SUCCESS);
+}
